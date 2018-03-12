2a1a758940689beae950adf1b1ab3beff9771db6
ajay <ajay.singh@tothenew.com>
2018-03-12 12:18:10 +0530

    Q4

--- src/main/java/com/ttn/cafe/Loggin.java
+++ src/main/java/com/ttn/cafe/Loggin.java
@@ -33,10 +33,44 @@ public class Loggin {
 //        return o;
 //    }
 
-    @AfterThrowing( pointcut = "execution(public void Exception())",throwing = "e")
-    public void advice(IllegalArgumentException e){
-        System.out.println ("After  Throwing exception" );
+//    @AfterThrowing( pointcut = "execution(public void Exception())",throwing = "e")
+//    public void advice(IllegalArgumentException e){
+//        System.out.println ("After  Throwing exception" );
+//    }
+    @Before("within(com.ttn.cafe.*)")
+    public void advice(){
+        System.out.println("Running before advice");
     }
 
+    @After("execution(public String getName())")
+    public void LoggingAdvice()
+{
+    System.out.println("I am in execution");
 }
+    @Before("execution(public String getName())")
+    public void LoggingAdvice1()
+    {
+        System.out.println("I am in execution");
+    }
+    @After("within(com.ttn.cafe.*)")
+    public void LoggingAdvice2()
+    {
+        System.out.println("I am in execution");
+    }
+    @Before("within(com.ttn.cafe.*)")
+    public void LoggingAdvice3()
+    {
+        System.out.println("I am in execution");
+    }
+    @Before("args(String)")
+    public void LoggingAdvice4()
+    {
+        System.out.println("I take string argument");
+    }
+    @AfterReturning("args(name)")
+    public void givename(String name){
+    System.out.println("this method takes argument as"+name);
+    }
+}
+
 
